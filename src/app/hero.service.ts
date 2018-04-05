643907ed4361c758f582f95e0f4f7d6489d3375f
Michael Huang <michaelkdh@gmail.com>
2018-04-05 22:48:57 +1000

    GS | Finish introduction to service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
